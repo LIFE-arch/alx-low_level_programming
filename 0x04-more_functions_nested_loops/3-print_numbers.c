708af1d1eb381f2e77db41c620769b8aaa613fff
LIFE-arch <agbajetayo63gbajetayo63@gmail.com>
2022-03-17 17:58:57 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - function that prints the numbers, from 0 to 9.
+*
+* Return: Always 0 (success).
+*/
+
+void print_numbers(void)
+{
+int 1;
+
+for (i = 0; i <= 9; i++)
+{
+_putchar('0' + 1);
+}
+_putchar('\n');
+}
