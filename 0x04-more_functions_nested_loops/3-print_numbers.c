d3c4d7939c7e8724fb4d830d1bd12452fa3d53ad
LIFE-arch <agbajetayo63gbajetayo63@gmail.com>
2022-03-17 18:05:22 -0700

    loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,16 @@
 /**
 * print_numbers - function that prints the numbers, from 0 to 9.
 *
-* Return: Always 0 (success).
+* Return: Always 0 (Success).
 */
 
 void print_numbers(void)
 {
-int 1;
+int i;
 
 for (i = 0; i <= 9; i++)
 {
-_putchar('0' + 1);
+_putchar('0' + i);
 }
 _putchar('\n');
 }
